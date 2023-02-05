9913eaa2bebcf94a0b668d6128e8cace2fb72900
ypankaj651@gmail.com <ypankaj651@gmail.com>
2023-02-05 20:06:09 +0530

    added recent message polling from telegram

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,6 +1,7 @@
 import requests
-import os
+import os, time, json
 from dotenv import load_dotenv
+from galaxy_battery_status import check_battery_status
 
 load_dotenv()   # initializing env vars from .env file
     
@@ -14,5 +15,34 @@ def send_to_telegram(message):
     try:
         response = requests.post(apiURL, json={'chat_id': chat_id, 'text': message})
         print("Sending to telegram: {}".format(response.status_code))
+    except Exception as e:
+        print(e)
+
+def same_msg(message_id):
+    try:
+        with open('data/last_message_id','r') as f:
+            last_message_id = int(f.readline())
+    except:
+        f = open('data/last_message_id','x')
+        f.close()
+        last_message_id = -1
+    if message_id == last_message_id:
+        return False
+    else:
+        with open('data/last_message_id','w') as f:
+            f.write(str(message_id))
+    return True
+
+def poll_telegram():
+    api = os.environ.get('api')
+    chat_id = os.environ.get('chat_id')
+    apiURL = f'https://api.telegram.org/bot{api}/getUpdates'
+    try:
+        response = requests.get(apiURL, json={'offset':-1})
+        message_data = response.json().get('result')[0].get('message')
+        last_message = message_data.get('text')
+        if same_msg(message_data.get('message_id')):
+            if last_message.lower()=='battery':
+                send_to_telegram(check_battery_status())
     except Exception as e:
         print(e)
