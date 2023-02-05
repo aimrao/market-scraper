3267ad80824030bebf27f0a4a13728e777835cdd
ypankaj651@gmail.com <ypankaj651@gmail.com>
2023-02-05 18:51:32 +0530

    fixed subprocess output in battery status code

--- galaxy_battery_status.py
+++ galaxy_battery_status.py
@@ -4,7 +4,7 @@ from telegram_bot import send_to_telegram
 
 cmd = 'cat /sys/class/power_supply/battery/capacity'
 try:
-    send_to_telegram('Galaxy Battery: ' + str(subprocess.run(cmd.split()).stdout))
+    send_to_telegram('Galaxy Battery: ' + str(subprocess.run(cmd.split(),  capture_output=True).stdout.decode('utf-8')+'%'))
 except Exception as e:
     send_to_telegram("Failed to get battery status.\n" + str(e))
 
