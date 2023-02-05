1f5d9a720ff464471918e0b0c54a535494154be1
ypankaj651@gmail.com <ypankaj651@gmail.com>
2023-02-05 20:06:33 +0530

    integrated battery check code with telegram polling

--- galaxy_battery_status.py
+++ galaxy_battery_status.py
@@ -1,11 +1,10 @@
 import subprocess
-from telegram_bot import send_to_telegram
 
-
-cmd = 'cat /sys/class/power_supply/battery/capacity'
-try:
-    send_to_telegram('Galaxy Battery: {}%'.format(str(subprocess.run(cmd.split(), capture_output=True).stdout.decode('utf-8').rstrip())))
-except Exception as e:
-    send_to_telegram("Failed to get battery status.\n" + str(e))
+def check_battery_status():
+    cmd = 'cat /sys/class/power_supply/battery/capacity'
+    try:
+        return 'Galaxy Battery: {}%'.format(str(subprocess.run(cmd.split(), capture_output=True).stdout.decode('utf-8').rstrip()))
+    except Exception as e:
+        return "Failed to get battery status.\n" + str(e)
 
 
